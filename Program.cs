a39decf7095f96dcc03c01f38e762026a3dd7072
Hans Christian Winther-Sorensen <hc@wsh.no>
2024-04-26 17:42:44 +0200

    Update to .net 8 and support for podman exe with compose argument

--- Program.cs
+++ Program.cs
@@ -1,46 +1,60 @@
 ﻿using System.Diagnostics;
 using System.Text.RegularExpressions;
 
-ProcessStartInfo processStartInfo = new() {
-    FileName = "podman-compose"
+ProcessStartInfo processStartInfo = new()
+{
+    FileName = "podman"
 };
-for (int i = 0; i < args.Length; i++) {
+
+for (var i = 0; i < args.Length; i++)
+{
     // Rewrite `--ansi never` to `--no-ansi`
-    if (args[i] == "--ansi") {
+    if (args[i] == "--ansi")
+    {
         ++i;
-        if (args[i] == "never") {
+        if (args[i] == "never")
             processStartInfo.ArgumentList.Add("--no-ansi");
-        }
+
         continue;
     }
+
     // Eat `--profile *`
-    if (args[i] == "--profile" && args[i + 1] == "*") {
+    if (args[i] == "--profile" && args[i + 1] == "*")
+    {
         ++i;
         continue;
     }
+
     // Eat `--rmi local` (https://github.com/containers/podman-compose/issues/387)
-    if (args[i] == "--rmi" && args[i + 1] == "local") {
+    if (args[i] == "--rmi" && args[i + 1] == "local")
+    {
         ++i;
         continue;
     }
+
     // Munge paths to Windows mounts (rewrite `C:\foo\bar` to `/foo/bar`)
-    if (args[i] == "-f" && args[i + 1].EndsWith(".g.yml")) {
-        string yamlPath = args[++i];
-        string yaml = File.ReadAllText(yamlPath);
+    if (args[i] == "-f" && args[i + 1]
+            .EndsWith(".g.yml"))
+    {
+        var yamlPath = args[++i];
+        var yaml = File.ReadAllText(yamlPath);
         yaml = Regex.Replace(yaml, @"- [cC]:\\([^:]+)", m => $"- /{m.Groups[1].Value.Replace(@"\", "/")}");
         File.WriteAllText(yamlPath + ".munged", yaml);
         processStartInfo.ArgumentList.Add("-f");
         processStartInfo.ArgumentList.Add(yamlPath + ".munged");
         continue;
     }
+
     // Rewrite `kill` to `kill --all`
-    if (args[i] == "kill" && i == args.Length - 1) {
+    if (args[i] == "kill" && i == args.Length - 1)
+    {
         processStartInfo.ArgumentList.Add("kill");
         processStartInfo.ArgumentList.Add("--all");
         break;
     }
+
     processStartInfo.ArgumentList.Add(args[i]);
 }
 
-Console.Error.WriteLine($"{processStartInfo.FileName} {string.Join(" ", processStartInfo.ArgumentList)}");
+Console.Error.WriteLine($"{processStartInfo.FileName} compose {string.Join(" ", processStartInfo.ArgumentList)}");
 Process.Start(processStartInfo)!.WaitForExit();
